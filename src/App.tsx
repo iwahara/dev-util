30c0a5135871dba69a46820b706531b22ac71a90
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2022-07-31 00:14:35 +0900

    command側の実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api/tauri';
 
 function App() {
+  function simpleCommands() {
+    invoke('simple_command')
+  }
+
+  function commandWithMessage() {
+    invoke('command_with_message', { message: 'some message' }).then(message => {
+      console.log('command_with_messge', message)
+    })
+  }
+  function commandWithObject() {
+    invoke('command_with_object', { message: { field_str: 'some message', field_u32: 12 }}).then(message => {
+      console.log('command_with_object', message)
+    })
+  }
+
+  function commandWithError() {
+    for (let arg of [1, 2]) {
+      invoke('command_with_error', { arg }).then(message => {
+        console.log('command_with_error', message)
+      }).catch(message => {
+        console.error('command_with_error', message)
+      })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <br></br>
+           Hello Tauri
         </p>
+        <button onClick={simpleCommands}>Click to execute simpleCommands</button>
+        <button onClick={commandWithMessage}>Click to execute commandWithMessage</button>
+        <button onClick={commandWithObject}>Click to execute commandWithObject</button>
+        <button onClick={commandWithError}>Click to execute commandWithError</button>
         <a
           className="App-link"
           href="https://reactjs.org"

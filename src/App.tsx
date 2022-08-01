edf7007f91a1c9c84a0166373b5cb97d314c7945
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2022-08-01 21:50:26 +0900

    見た目を整えた

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import theme from './theme';
 import CronParser from './components/CronParser'
 import { Container } from '@chakra-ui/react'
 import { Grid, GridItem } from '@chakra-ui/react'
+import SimpleSidebar from './components/SideBar';
 
 class App extends React.Component {
 
@@ -14,29 +15,7 @@ class App extends React.Component {
     return (
       <ChakraProvider>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <Grid
-          templateAreas={`"header header"
-                          "nav main"
-                          "nav footer"`}
-          gridTemplateRows={'50px 1fr 30px'}
-          gridTemplateColumns={'150px 1fr'}
-          h='200px'
-          gap='1'
-          color='blackAlpha.700'
-          fontWeight='bold'>
-          <GridItem pl='2' bg='orange.300' area={'header'}>
-            Header
-          </GridItem>
-          <GridItem pl='2' bg='pink.300' area={'nav'}>
-            Nav
-          </GridItem>
-          <GridItem pl='2' bg='blue.300' area={'main'}>
-            <CronParser />
-          </GridItem>
-          <GridItem pl='2' bg='blue.300' area={'footer'}>
-            Footer
-          </GridItem>
-        </Grid>
+        <SimpleSidebar children={<CronParser />} />
       </ChakraProvider>
     );
   }

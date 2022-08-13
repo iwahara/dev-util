2a6e723b37e05a6a5e51ff1b7a6ac863439ae937
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2022-08-14 01:56:36 +0900

    画面の遷移できた

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import './App.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import { ColorModeScript } from '@chakra-ui/react'
-import theme from './theme';
-import CronParser from './components/CronParser'
-import SimpleSidebar from './components/SideBar';
-import CidrAnalyzer from './components/CidrAnalyzer';
+import React from "react";
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
+import CronParser from "./components/CronParser";
+import SimpleSidebar from "./components/SideBar";
+import CidrAnalyzer from "./components/CidrAnalyzer";
 
-class App extends React.Component {
-
-  render(){
-    return (
-      <ChakraProvider>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <SimpleSidebar children={<CidrAnalyzer />} />
-      </ChakraProvider>
-    );
-  }
+function App() {
+  return (
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <SimpleSidebar />
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- src/components/CidrAnalyzer.tsx
+++ src/components/CidrAnalyzer.tsx
@@ -1,153 +1,199 @@
-import { HStack, VStack, Input, InputGroup, InputRightAddon, NumberInput, NumberInputField, Button, useToast } from "@chakra-ui/react";
-import { useState } from "react";
-import { invoke } from '@tauri-apps/api/tauri';
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
-
+  HStack,
+  VStack,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  NumberInput,
+  NumberInputField,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 interface CidrAnalyzerRequest {
-    first_octet: Number;
-    second_octet: Number;
-    third_octet: Number;
-    fourth_octet: Number;
-    prefix_len: Number;
+  first_octet: Number;
+  second_octet: Number;
+  third_octet: Number;
+  fourth_octet: Number;
+  prefix_len: Number;
 }
 
 interface CidrAnalyzerResponse {
-    formatted_ip: String,
-    formatted_cidr: String,
-    subnet: String,
-    ip_address_range_start: String,
-    ip_address_range_end: String,
-    address_range_length: Number,
-    network_ip_address: String,
-    broadcast_ip_address: String,
+  formatted_ip: String;
+  formatted_cidr: String;
+  subnet: String;
+  ip_address_range_start: String;
+  ip_address_range_end: String;
+  address_range_length: Number;
+  network_ip_address: String;
+  broadcast_ip_address: String;
 }
 
-function CidrAnalyzer(){
-    const [buttonDisabled, setButtonDisabled] = useState(false);
-    const [firstOctet, setFirstOctet] = useState(0);
-    const [secondOctet, setSecondOctet] = useState(0);
-    const [thirdOctet, setThirdOctet] = useState(0);
-    const [fourthOctet, setFourthOctet] = useState(0);
-    const [prefixLen, setPrefixLen] = useState(0);
-    const [response, setResponse] = useState<CidrAnalyzerResponse>();
+function CidrAnalyzer() {
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [firstOctet, setFirstOctet] = useState(0);
+  const [secondOctet, setSecondOctet] = useState(0);
+  const [thirdOctet, setThirdOctet] = useState(0);
+  const [fourthOctet, setFourthOctet] = useState(0);
+  const [prefixLen, setPrefixLen] = useState(0);
+  const [response, setResponse] = useState<CidrAnalyzerResponse>();
 
-    const toast = useToast()
+  const toast = useToast();
 
-    function commandCidrAnalyzer(){
-        setButtonDisabled(true);
-        
-        const req  = {
-            req:{
-                first_octet: firstOctet,
-                second_octet: secondOctet,
-                third_octet: thirdOctet,
-                fourth_octet: fourthOctet,
-                prefix_len: prefixLen,
-            }
-        };
-        invoke<CidrAnalyzerResponse>('command_cidr_analyzer', req).then(res => {
-            console.log(res);
-            setResponse(res);
-        }).catch(message => {
-            console.error('command_cidr_analyzer', message);
-            toast({
-                title: 'CIDR 解析エラー',
-                description: message,
-                status: 'error',
-                duration: 5000,
-                isClosable: true,
-              })
-        }).finally(() =>{
-            setButtonDisabled(false);
+  function commandCidrAnalyzer() {
+    setButtonDisabled(true);
+
+    const req = {
+      req: {
+        first_octet: firstOctet,
+        second_octet: secondOctet,
+        third_octet: thirdOctet,
+        fourth_octet: fourthOctet,
+        prefix_len: prefixLen,
+      },
+    };
+    invoke<CidrAnalyzerResponse>("command_cidr_analyzer", req)
+      .then((res) => {
+        console.log(res);
+        setResponse(res);
+      })
+      .catch((message) => {
+        console.error("command_cidr_analyzer", message);
+        toast({
+          title: "CIDR 解析エラー",
+          description: message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
         });
-    }
+      })
+      .finally(() => {
+        setButtonDisabled(false);
+      });
+  }
 
-    return (
-        <div>
-            <VStack align="left">
-                <InputGroup>
-                    <NumberInput min={0} max={255} onChange={(value) => setFirstOctet(Number(value))}>
-                        <NumberInputField />
-                    </NumberInput>
-                    <InputRightAddon children='.' />
-                    <NumberInput min={0} max={255} onChange={(value) => setSecondOctet(Number(value))}>
-                        <NumberInputField />
-                    </NumberInput>
-                    <InputRightAddon children='.' />
-                    <NumberInput min={0} max={255} onChange={(value) => setThirdOctet(Number(value))}>
-                        <NumberInputField />
-                    </NumberInput>
-                    <InputRightAddon children='.' />
-                    <NumberInput min={0} max={255} onChange={(value) => setFourthOctet(Number(value))}>
-                        <NumberInputField />
-                    </NumberInput>
-                    <InputRightAddon children='/' />
-                    <NumberInput min={0} max={32} onChange={(value) => setPrefixLen(Number(value))}>
-                        <NumberInputField />
-                    </NumberInput>
-                </InputGroup>
-                <Button onClick={commandCidrAnalyzer} colorScheme='blue' disabled={buttonDisabled}>解析する</Button>
-                {response &&
-                    <TableContainer>
-                        <Table variant='simple' colorScheme="blue">
-                            <Thead>
-                            <Tr>
-                                <Th>項目</Th>
-                                <Th>値</Th>
-                            </Tr>
-                            </Thead>
-                            <Tbody>
-                            <Tr>
-                                <Td>IPアドレス</Td>
-                                <Td>{response?.formatted_ip}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>CIDR</Td>
-                                <Td>{response?.formatted_cidr}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>サブネット</Td>
-                                <Td>{response?.subnet}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>IPアドレスの範囲</Td>
-                                <Td>{response?.ip_address_range_start} - {response?.ip_address_range_end}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>IPアドレス数</Td>
-                                <Td>{response?.address_range_length.toString()}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>ネットワークIPアドレス</Td>
-                                <Td>{response?.network_ip_address}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>ブロードキャストIPアドレス</Td>
-                                <Td>{response?.broadcast_ip_address}</Td>
-                            </Tr>
-                            </Tbody>
-                            <Tfoot>
-                            <Tr>
-                                <Th>項目</Th>
-                                <Th>値</Th>
-                            </Tr>
-                            </Tfoot>
-                        </Table>
-                    </TableContainer>
-                }
-            </VStack>
-        </div>
-    );
+  return (
+    <div>
+      <VStack align="left">
+        <InputGroup>
+          <NumberInput
+            defaultValue={0}
+            min={0}
+            max={255}
+            onChange={(value) => setFirstOctet(Number(value))}
+          >
+            <NumberInputField />
+          </NumberInput>
+          <InputRightAddon children="." />
+          <NumberInput
+            defaultValue={0}
+            min={0}
+            max={255}
+            onChange={(value) => setSecondOctet(Number(value))}
+          >
+            <NumberInputField />
+          </NumberInput>
+          <InputRightAddon children="." />
+          <NumberInput
+            defaultValue={0}
+            min={0}
+            max={255}
+            onChange={(value) => setThirdOctet(Number(value))}
+          >
+            <NumberInputField />
+          </NumberInput>
+          <InputRightAddon children="." />
+          <NumberInput
+            defaultValue={0}
+            min={0}
+            max={255}
+            onChange={(value) => setFourthOctet(Number(value))}
+          >
+            <NumberInputField />
+          </NumberInput>
+          <InputRightAddon children="/" />
+          <NumberInput
+            defaultValue={0}
+            min={0}
+            max={32}
+            onChange={(value) => setPrefixLen(Number(value))}
+          >
+            <NumberInputField />
+          </NumberInput>
+        </InputGroup>
+        <Button
+          onClick={commandCidrAnalyzer}
+          colorScheme="blue"
+          disabled={buttonDisabled}
+        >
+          解析する
+        </Button>
+        {response && (
+          <TableContainer>
+            <Table variant="simple" colorScheme="blue">
+              <Thead>
+                <Tr>
+                  <Th>項目</Th>
+                  <Th>値</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>IPアドレス</Td>
+                  <Td>{response?.formatted_ip}</Td>
+                </Tr>
+                <Tr>
+                  <Td>CIDR</Td>
+                  <Td>{response?.formatted_cidr}</Td>
+                </Tr>
+                <Tr>
+                  <Td>サブネット</Td>
+                  <Td>{response?.subnet}</Td>
+                </Tr>
+                <Tr>
+                  <Td>IPアドレスの範囲</Td>
+                  <Td>
+                    {response?.ip_address_range_start} -{" "}
+                    {response?.ip_address_range_end}
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>IPアドレス数</Td>
+                  <Td>{response?.address_range_length.toString()}</Td>
+                </Tr>
+                <Tr>
+                  <Td>ネットワークIPアドレス</Td>
+                  <Td>{response?.network_ip_address}</Td>
+                </Tr>
+                <Tr>
+                  <Td>ブロードキャストIPアドレス</Td>
+                  <Td>{response?.broadcast_ip_address}</Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>項目</Th>
+                  <Th>値</Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+          </TableContainer>
+        )}
+      </VStack>
+    </div>
+  );
 }
 export default CidrAnalyzer;
